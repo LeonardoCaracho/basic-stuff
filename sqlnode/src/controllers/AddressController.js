0359d932726b8ff4e2c66b3e63793a30f26c2da5
negreyscow <leeo.apc@gmail.com>
2020-05-30 16:28:23 -0300

    messing around

--- sqlnode/src/controllers/AddressController.js
+++ sqlnode/src/controllers/AddressController.js
@@ -2,6 +2,18 @@ const Address = require('../models/Address')
 const User = require('../models/User')
 
 module.exports = {
+    async index(req, res){
+        const { user_id } = req.params
+
+        const user = await User.findByPk(user_id, {
+            include: {
+                association: 'addresses'
+            }
+        })
+
+        return res.json(user.addresses)
+    },
+    
     async store(req, res){
         const { user_id } = req.params
         const { zipcode, street, number } = req.body
